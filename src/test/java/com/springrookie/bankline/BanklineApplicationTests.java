03f6562db250bbdea9c3e03d659d497daff0f3fa
Junior Vieira <junior.vieira.1990@gmail.com>
2022-09-10 12:34:06 -0300

    :tada: Commit inicial

---
+++ src/test/java/com/springrookie/bankline/BanklineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springrookie.bankline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BanklineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
